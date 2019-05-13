7db1c6dc9f1c93b75ab220335c9573d487843854
IwonaWilk <iwona.k.bialecka@gmail.com>
2019-05-13 16:26:24 +0100

    Yelm mvp exercise rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+Review.destroy_all
+
+cat = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Name.unique.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: cat.sample
+  )
+  review = Review.new(
+    restaurant: restaurant,
+    content: Faker::TvShows::FamilyGuy.quote,
+    rating: rand(0..5)
+    )
+  restaurant.save!
+  review.save!
+end
